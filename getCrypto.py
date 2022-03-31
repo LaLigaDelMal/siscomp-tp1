d25e6610f711e3863437ac8d16c011f76569dd5d
Gaspar Segura <gaspar_segura_97@hotmail.com>
2022-03-30 23:05:12 -0300

    Se agreagan criptos y valor del peso

--- getCrypto.py
+++ getCrypto.py
@@ -1,15 +1,25 @@
 import requests
 
-URL = "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
-
-
+URL_BTC = "https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT"
+URL_ETH = "https://api.binance.com/api/v3/avgPrice?symbol=ETHUSDT"
+URL_ARG = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
 
 def main():
-    r = requests.get(URL)
-    data = r.json()
+    btc_response = requests.get(URL_BTC)
+    data_btc = btc_response.json()
+    btc = data_btc['price']
 
-    print(data['price'])
+    eth_response = requests.get(URL_ETH)
+    data_eth = eth_response.json()
+    eth = data_eth['price']
 
+    arg_response = requests.get(URL_ARG)
+    data_arg = arg_response.json()
+    arg = data_arg[1]['casa']['compra'][:-1]
+
+    print (btc)
+    print (eth)
+    print (arg)
 
 
 if __name__ == "__main__":
